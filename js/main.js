af3e2db276e147dbb3356a373b88f5a25bdc8dee
troisterre <a.kamimura.troisterre@gmail.com>
2024-11-08 11:47:05 +0900

    11/8  課題修正

--- js/main.js
+++ js/main.js
@@ -4,12 +4,13 @@ $("#js-btn").on("click", function () {
   //activeがある時の処理
   if ($(this).hasClass("active")) {
     $(this).removeClass("active"); //activeを削除
-
     $(".l-sidebar").removeClass("open"); //#js-menuの.openを削除
+    $(".l-main__left").removeClass("fixed");
   } else {
     $(this).addClass("active"); //activeを追加
     $("#js-bg").fadeIn("fast");
     $(".l-sidebar").addClass("open");
+    $(".l-main__left").addClass("fixed");
   }
 });
 
